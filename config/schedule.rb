7bd8c306ab5f0cc98ed080994170d79075788fe1
luyingrui <luyingrui@oneapm.com>
2016-03-19 15:06:59 +0800

    refactor schedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,36 +2,19 @@
 #
 # It's helpful, but not entirely necessary to understand cron before proceeding.
 # http://en.wikipedia.org/wiki/Cron
-
-# Example:
-#
- app_root = File.expand_path('../', File.dirname(__FILE__))
-
- set :output, "#{app_root}/cron_log.log"
-
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
 # Learn more: http://github.com/javan/whenever
 
-ruby_path =  `which ruby`.chop
-job_type  :sh, "cd #{app_root} && #{ruby_path} :task :output"
+ set :app_path, File.expand_path('../', File.dirname(__FILE__))
+ set :output, "log/crontab.log"
 
+ruby_path =  `which ruby`.chop
+job_type  :sh, "cd :app_path && #{ruby_path} :task :output"
 
 every 1.minute do 
-   sh "lib/weather.rb"
+  sh "lib/weather.rb"
 end
 
-
- every 5.minutes  do
-   sh "lib/ruby_china.rb" 
- end
+every 5.minutes  do
+  sh "lib/ruby_china.rb" 
+end
 
